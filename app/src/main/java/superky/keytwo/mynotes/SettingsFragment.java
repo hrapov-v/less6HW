683f35376661bf747081227b87bba7c99ab76cce
hrapov-v <hrapov-v@bk.ru>
2021-07-03 14:19:23 +0300

    Смотрю как будет работать приложение по коспекту.
    Чтобы потом доработать под свои цели, поэтому в сеттингс в фрагменте не выносил всё в ресурсы.

--- app/src/main/java/superky/keytwo/mynotes/SettingsFragment.java
+++ app/src/main/java/superky/keytwo/mynotes/SettingsFragment.java
@@ -13,6 +13,11 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
     }
 }
