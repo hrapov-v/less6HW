63328582841531dd359d7e6a4cdecd65e5037666
hrapov-v <hrapov-v@bk.ru>
2021-07-03 15:14:26 +0300

    Чисто конспект Делаю обработчики кнопок

--- app/src/main/java/superky/keytwo/mynotes/MainActivity.java
+++ app/src/main/java/superky/keytwo/mynotes/MainActivity.java
@@ -3,6 +3,8 @@ package superky.keytwo.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        Button buttonBack = findViewById(R.id.btnBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Button buttonMain = findViewById(R.id.btnMain);
+        Button buttonSettings = findViewById(R.id.btnStgns);
+        Button buttonInfo = findViewById(R.id.btnInfo);
+
     }
 }
