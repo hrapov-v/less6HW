eb6a90f51e61c5a5195fc2c6426cd3e3c94cc005
hrapov-v <hrapov-v@bk.ru>
2021-07-03 15:47:41 +0300

    По конспекту Сделал обработку кнопки notes но при повороте экрана активити Land не включается вылетает.

--- app/src/main/java/superky/keytwo/mynotes/MainActivity.java
+++ app/src/main/java/superky/keytwo/mynotes/MainActivity.java
@@ -1,10 +1,15 @@
 package superky.keytwo.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         Button buttonMain = findViewById(R.id.btnMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if(Settings.isDeleteBeforeAdd) {
+                    List<Fragment> fragmentList = fragmentManager.getFragments();
+                    for(int i = 0; i < fragmentList.size(); i++) {
+                        Fragment fragment = fragmentList.get(i);
+                        if(fragment.isVisible()) {
+                            fragmentTransaction.remove(fragment);
+                        }
+                    }
+                }
+                //if(Settings.isAddFragment) {
+                    fragmentTransaction.add(R.id.container_fragment, new MainFragment());
+                //}
+                fragmentTransaction.commit();
+            }
+        });
         Button buttonSettings = findViewById(R.id.btnStgns);
         Button buttonInfo = findViewById(R.id.btnInfo);
 
