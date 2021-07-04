28bc7edb9c506910b4599c79ebb81ff048724211
hrapov-v <hrapov-v@bk.ru>
2021-07-04 14:04:34 +0300

    Убрал action bar

--- app/src/main/java/superky/keytwo/mynotes/MainActivity.java
+++ app/src/main/java/superky/keytwo/mynotes/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     fragmentManager.popBackStack();
                 }
+                fragmentTransaction.commit();
             }
         });
     }
