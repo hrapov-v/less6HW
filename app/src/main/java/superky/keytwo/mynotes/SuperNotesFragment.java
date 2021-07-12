581c0c45a818daf1242d4ee1667a5f229ff4cb42
hrapov-v <hrapov-v@bk.ru>
2021-07-12 23:17:44 +0300

    Сделал меню Оно отображается когда открываем фрагмент заметок

--- app/src/main/java/superky/keytwo/mynotes/SuperNotesFragment.java
+++ app/src/main/java/superky/keytwo/mynotes/SuperNotesFragment.java
@@ -2,9 +2,12 @@ package superky.keytwo.mynotes;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +16,10 @@ import superky.keytwo.mynotes.data.CardSourceImpl;
 
 
 public class SuperNotesFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private CardSourceImpl data;
+    private SuperNotesAdapter superNotesAdapter;
 
     public static SuperNotesFragment newInstance() {
         return new SuperNotesFragment();
@@ -23,19 +30,28 @@ public class SuperNotesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_super_notes, container, false);
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView = view.findViewById(R.id.recyclerView);
         initList(recyclerView);
+        //ЗДЕСЬ важно этот метод указывает на то что наш фрагмент имеет собственное меню.
+        setHasOptionsMenu(true);
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        //Запихиваем это меню в контейнер меню который приходит в этом методе.
+        inflater.inflate(R.menu.fragment_menu, menu);
+    }
+
     private void initList(RecyclerView recyclerView) {
-        CardSourceImpl data = new CardSourceImpl(getResources());
+        data = new CardSourceImpl(getResources());
         data.init();
         recyclerView.setHasFixedSize(true);
         //layoutManager их три вида линейный шахматный и ещё один.....
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext()); //указал в макете но оставил чисто как в уроке
         recyclerView.setLayoutManager(layoutManager);
-        SuperNotesAdapter superNotesAdapter = new SuperNotesAdapter(data);
+        superNotesAdapter = new SuperNotesAdapter(data);
         superNotesAdapter.SetOnMyClickListenner(new OnMyClickListenner() {
             @Override
             public void onMyClick(View view, int position) {
--- app/src/main/java/superky/keytwo/mynotes/data/CardSourceImpl.java
+++ app/src/main/java/superky/keytwo/mynotes/data/CardSourceImpl.java
@@ -16,7 +16,7 @@ public class CardSourceImpl implements CardSource {
         dataSource = new ArrayList<>();
     }
 
-    public List<CardData> init(){
+    public List<CardData> init() {
         String[] notes = resources.getStringArray(R.array.notes);
         String[] noteBody = resources.getStringArray(R.array.noteBody);
         for (int i = 0; i < notes.length; i++) {
@@ -47,17 +47,17 @@ public class CardSourceImpl implements CardSource {
 
     @Override
     public void deleteCardData(int position) {
-
+        dataSource.remove(position);
     }
 
     @Override
     public void updateCardData(int position, CardData cardData) {
-
+        dataSource.set(position, cardData);
     }
 
     @Override
     public void clearCardData() {
-
+        dataSource.clear();
     }
 
     public List<CardData> getDataSource() {
