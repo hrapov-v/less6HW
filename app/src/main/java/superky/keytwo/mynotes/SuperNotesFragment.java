dfeb32df8fcde1e412c0b3f7a0a1ca8893af18c5
hrapov-v <hrapov-v@bk.ru>
2021-07-13 21:58:16 +0300

    Добавил метод который исправляет баг
    С помощью преподавателя, баг заключался в том, что при удалении всех заметок новую создать было нельзя, Андрей предоставил мне два метода для решения этой проблемы я выбрал первый
    PS: Много букав чтобы запомнить свою ошибку и метод решения

--- app/src/main/java/superky/keytwo/mynotes/SuperNotesFragment.java
+++ app/src/main/java/superky/keytwo/mynotes/SuperNotesFragment.java
@@ -64,8 +64,10 @@ public class SuperNotesFragment extends Fragment {
         switch (item.getItemId()) {
             case R.id.action_add:
                 data.addCardData(new CardData("Как то иначе " + (data.size() + 1), "Как то иначе описание " + (data.size() + 1)));
+                //Заново задаём связь списка с адаптером
+                recyclerView.setAdapter(superNotesAdapter);
                 // обновляем позицию data.size() - 1
-                superNotesAdapter.notifyItemInserted(data.size() - 1);
+                //superNotesAdapter.notifyItemInserted(data.size() - 1);
                 //метод позволяющий скролить до нужной позиции
                 recyclerView.smoothScrollToPosition(data.size() - 1);
                 return true;
